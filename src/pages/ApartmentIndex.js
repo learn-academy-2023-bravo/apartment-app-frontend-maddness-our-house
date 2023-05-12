be199d94d786029cb3db53fb6a76abf3964a7808
raymondkneipp <raymond.f.kneipp@gmail.com>
2023-05-12 09:40:11 -0700

    add grid layout and styles

--- src/pages/ApartmentIndex.js
+++ src/pages/ApartmentIndex.js
@@ -1,47 +1,52 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { Card, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap"
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Row,
+  Col,
+} from 'reactstrap'
 
-const ApartmentIndex = ( { apartments }) => {
-  const handleClick = () => {
-
-  }
-  return(
+const ApartmentIndex = ({ apartments }) => {
+  return (
     <>
       <h1>Apartments Listings</h1>
-      {apartments.map((apartment, index) => {
-        return(
-          
-          <Card style={{ width: '18rem' }} key={index}
-          >
-            <img
-              alt={`profile of apartment listing ${apartment.street}`}
-              src={apartment.image}
-            />
-            <CardBody>
-              <CardTitle tag="h5">
-                {apartment.street}
-              </CardTitle>
-              <CardSubtitle
-                className="mb-2 text-muted"
-                tag="h6"
-              >
-                {`$ ${apartment.price}`}
-              </CardSubtitle>
-                <Button 
-                data-testid={apartment.id}
-                id="button"
-                name="Listing Details"
-                className="self-end"
-                to={`/apartmentshow/${apartment.id}`}
-                tag={NavLink}
-                >
-                  Listing Details
-                </Button>
-            </CardBody>
-          </Card>
-        )
-      })}
+      <Row>
+        {apartments.map((apartment, index) => {
+          return (
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <Card key={index}>
+                <img
+                  alt={`profile of apartment listing ${apartment.street}`}
+                  src={apartment.image}
+                  className='object-fit-cover'
+                  width='100%'
+                  height='200px'
+                />
+                <CardBody>
+                  <CardTitle tag='h5'>{apartment.street}</CardTitle>
+                  <CardSubtitle className='mb-2 text-muted' tag='h6'>
+                    {`$ ${apartment.price}`}
+                  </CardSubtitle>
+                  <Button
+                    data-testid={apartment.id}
+                    id='button'
+                    name='Listing Details'
+                    className='self-end'
+                    to={`/apartmentshow/${apartment.id}`}
+                    tag={NavLink}
+                  >
+                    Listing Details
+                  </Button>
+                </CardBody>
+              </Card>
+            </Col>
+          )
+        })}
+      </Row>
     </>
   )
 }
